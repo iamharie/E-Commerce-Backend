630a696b3927c676d0bd089e4c9e27b14fbb6645
Hariharan Mohan <haranhari350@gmail.com>
2025-01-10 12:59:26 -0600

    init; initial project push

---
+++ src/main/java/com/hari/e_commerce/ECommerceApiApplication.java
@@ -0,0 +1,16 @@
+package com.hari.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//Spring starts executing from this class
+@SpringBootApplication
+public class ECommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApiApplication.class, args);
+
+	}
+
+}
